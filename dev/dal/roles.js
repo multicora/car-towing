a86d403ad61798bda41e64fc23364be17d08d373
Feditskiy Roman <feditskiy@gmail.com>
2016-08-17 11:57:46 +0300

    feature/CART-15 define roles table

---
+++ dev/dal/roles.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const path = require('path');
+
+const Schema = mongoose.Schema;
+const schema = new Schema({
+  roles: String
+});
+
+let model = mongoose.model(path.basename(module.filename, '.js'), schema);
+
+module.exports = model;

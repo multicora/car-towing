bf7f7dcbd24781b8bb1068367e99a7f77a810875
Feditskiy Roman <feditskiy@gmail.com>
2016-08-23 11:25:14 +0300

    CART-75 added and fixed menu

--- dev/publicDev/buttons/buttonsConfig.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var app = angular.module('app');
-
-app.
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/buttons', {templateUrl: 'buttons/buttons.html'});
-}]);

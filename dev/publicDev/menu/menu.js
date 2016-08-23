3ff2f8f4d0fe82daacff7f8902b5ec2a20584665
Feditskiy Roman <feditskiy@gmail.com>
2016-08-23 11:42:33 +0300

    CART-75 added admin menu directive

--- dev/publicDev/menu/menu.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var app = angular.module('app');
-
-app.
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/admin', {templateUrl: 'menu/menu.html'});
-}]);

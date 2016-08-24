5879bc16a2b843b636727f3e9466feb68de0ff68
Feditskiy Roman <feditskiy@gmail.com>
2016-08-24 17:05:24 +0300

    CART-83 little fix

--- dev/publicDev/property.page/propertyPageConfig.js
+++ dev/publicDev/property.page/propertyPageConfig.js
@@ -3,7 +3,7 @@
 var app = angular.module('app');
 
 app.
-config(['$routeProvider', function($routeProvider) {;
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/property:id', {
     templateUrl: 'property.page/propertyPage.html',
     controller: 'propertyCtrl as vm'

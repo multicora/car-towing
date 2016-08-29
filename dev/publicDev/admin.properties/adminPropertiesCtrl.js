516d341162e0e192999696315d48606917668a68
Feditskiy Roman <feditskiy@gmail.com>
2016-08-29 10:50:20 +0300

    CART-66 added logical part

--- dev/publicDev/admin.properties/adminPropertiesCtrl.js
+++ dev/publicDev/admin.properties/adminPropertiesCtrl.js
@@ -4,9 +4,27 @@
   angular.module('app')
     .controller('adminPropertiesCtrl', ctrl);
 
-  ctrl.$inject = [];
+  ctrl.$inject = ['propertyService', 'DataService'];
 
-  function ctrl() {
+  function ctrl(propertyService, DataService) {
     var vm = this;
+    vm.login = ''; 
+    vm.name = '';
+
+    vm.addProperty = addProperty;
+
+    function addProperty() {
+      DataService.login(vm.login)
+      .then((success) => {
+        propertyService.postProperty(vm.name)
+          .then((success) => {
+            console.log(success);
+          }, (error) => {
+            console.log(error);
+          });
+      }, (error) => {
+        console.log(error);
+      });
+    }
   }
 })();
--- dev/publicDev/property.page/propertyPageService.js
+++ dev/publicDev/property.page/propertyPageService.js
@@ -7,6 +7,12 @@ app.factory('propertyService', function($http) {
     },
     getRules: function(id) {
       return $http.get('/api/parkingRules/' + id)
+    },
+    postProperty: function(data) {
+      return $http.post('/api/property', data)
+    }
+    editProprty: function(id) {
+      return $http.put('/api/property/' + id)
     }
   }
 });

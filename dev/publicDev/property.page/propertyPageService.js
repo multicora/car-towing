8d1957d5d8a00755e53b31d577955bc89be79a2f
Feditskiy Roman <feditskiy@gmail.com>
2016-08-29 10:51:42 +0300

    CART-66 little fixed

--- dev/publicDev/property.page/propertyPageService.js
+++ dev/publicDev/property.page/propertyPageService.js
@@ -10,7 +10,7 @@ app.factory('propertyService', function($http) {
     },
     postProperty: function(data) {
       return $http.post('/api/property', data)
-    }
+    },
     editProprty: function(id) {
       return $http.put('/api/property/' + id)
     }

554d32436211ed200f3d6970f0a8a07f0c7fd58c
vadim <vadim.nagirniak@aiesec.net>
2016-08-31 10:04:39 +0300

    CART-64/65 admin.property

--- dev/publicDev/admin.properties/adminPropertiesCtrl.js
+++ dev/publicDev/admin.properties/adminPropertiesCtrl.js
@@ -2,11 +2,28 @@
 
 (() => {
   angular.module('app')
-    .controller('adminPropertiesCtrl', ctrl);
+    .controller('adminPropertiesCtrl', adminPropertiesCtrl);
 
-  ctrl.$inject = [];
+  adminPropertiesCtrl.$inject = ['adminPropertiesService'];
 
-  function ctrl() {
+  function adminPropertiesCtrl(adminPropertiesService) {
     var vm = this;
+		vm.adminProperties = [];
+		vm.emptyMess = '';
+		vm.errorMess = '';
+		vm.logo = '/files/property.png';
+		vm.getAdminProperties = () => {
+			adminPropertiesService.getAdminProperties()
+				.then((success) => {
+					console.log(success);
+					vm.adminProperties = success.data;
+					if(vm.adminProperties.length) {
+						vm.emptyMess = 'Any properties';
+					}
+				}, (error) => {
+					console.error(error);
+					vm.errorMess = error.data.error;
+				});
+		} 
   }
 })();

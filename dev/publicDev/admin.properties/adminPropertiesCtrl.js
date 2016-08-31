7075cb8f97d14546006e67b27020ba75598d98a7
vadim <vadim.nagirniak@aiesec.net>
2016-08-31 23:28:20 +0300

    fix space

--- dev/publicDev/admin.properties/adminPropertiesCtrl.js
+++ dev/publicDev/admin.properties/adminPropertiesCtrl.js
@@ -2,21 +2,21 @@
 
 (() => {
   angular
-  	.module('app')
+    .module('app')
     .controller('adminPropertiesCtrl', adminPropertiesCtrl);
 
   adminPropertiesCtrl.$inject = ['adminPropertiesService'];
 
   function adminPropertiesCtrl(adminPropertiesService) {
     var vm = this;
-		vm.adminProperties = [];
-		vm.getAdminProperties = () => {
-			adminPropertiesService.getAdminProperties()
-				.then((success) => {
-					vm.adminProperties = success.data;
-				}, (error) => {
-					console.error(error);
-				});
-		} 
+    vm.adminProperties = [];
+    vm.getAdminProperties = () => {
+      adminPropertiesService.getAdminProperties()
+        .then((success) => {
+          vm.adminProperties = success.data;
+        }, (error) => {
+          console.error(error);
+        });
+    } 
   }
 })();

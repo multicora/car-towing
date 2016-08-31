4475cf15130bedce3b6554dbb1df488dc1733c36
vetalcore <vetalcore@gmail.com>
2016-09-01 01:12:30 +0300

    CART-68 Possibility to upload logo for property

--- dev/publicDev/admin.properties/adminPropertiesCtrl.js
+++ dev/publicDev/admin.properties/adminPropertiesCtrl.js
@@ -4,17 +4,31 @@
   angular.module('app')
     .controller('adminPropertiesCtrl', ctrl);
 
-  ctrl.$inject = ['propertyService', '$location'];
+  ctrl.$inject = ['propertyService', '$location', '$scope'];
 
-  function ctrl(propertyService, $location) {
+  function ctrl(propertyService, $location, $scope) {
     var vm = this;
     vm.login = ''; 
-    vm.name = '';
     vm.errorMes = '';
+    vm.newProperty = {};
+
+    vm.selectFile = function() {
+      document.getElementById('file').click();
+    };
+
+    $scope.convertToBase64 = function(event){
+      var f = document.getElementById('file').files[0],
+      r = new FileReader();
+      r.onloadend = function(e){
+        // TODO: converted to base64 image
+        vm.newProperty.logo = e.target.result;
+      }
+      r.readAsDataURL(f);
+    }
 
     vm.addProperty = function(form) {
       if (form.$valid) {
-        propertyService.create(vm.name)
+        propertyService.create(vm.newProperty)
         .then((success) => {
           $location.path('/admin/properties');
         }, (error) => {

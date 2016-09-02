704ee67c75be4318276f2fad92fcdbe450cc2560
Feditskiy Roman <feditskiy@gmail.com>
2016-09-02 16:05:59 +0300

    CART-27 fixed

--- dev/routing.js
+++ dev/routing.js
@@ -101,24 +101,10 @@ module.exports.init = function (server) {
   server.route({
     method: 'GET',
     path: '/api/properties',
-    config: { 
-      pre: [
-        { method: 'checkTokin(raw.req.headers.token)', assign: "token" }
-      ],
-      validate: {
-        params: {
-          name: Joi.string().min(1).max(255).required(),
-          address: Joi.string(),
-          objId: Joi.string(),
-          logo: Joi.string(),
-          rules: Joi.array().items(Joi.string())
-        }
-      },
-      handler: function (request, reply) {
-        DAL.properties.get(function (err, docs) {
-          !err ? reply(docs) : reply(JSON.stringify(err));
-        });
-      }
+    handler: function (request, reply) {
+      DAL.properties.get(function (err, docs) {
+        !err ? reply(docs) : reply(JSON.stringify(err));
+      });
     }
   });
   server.route({
@@ -128,15 +114,6 @@ module.exports.init = function (server) {
       pre: [
         { method: 'checkTokin(raw.req.headers.token)', assign: "token" }
       ],
-      validate: {
-        params: {
-          name: Joi.string().min(1).max(255).required(),
-          address: Joi.string(),
-          objId: Joi.string(),
-          logo: Joi.string(),
-          rules: Joi.array().items(Joi.string())
-        }
-      },
       handler: function (request, reply) {
         DAL.properties.getById(request.params.id, function (err, docs) {
           !err ? reply(docs) : reply(JSON.stringify(err));
@@ -189,21 +166,10 @@ module.exports.init = function (server) {
   server.route({
     method: 'DELETE',
     path: '/api/property/{id}',
-    config: {
-      validate: {
-        params: {
-          name: Joi.string().min(1).max(255).required(),
-          address: Joi.string(),
-          objId: Joi.string(),
-          logo: Joi.string(),
-          rules: Joi.array().items(Joi.string())
-        }
-      },
-      handler: function (request, reply) {
-        DAL.properties.remove(request.params.id, function (err, docs) {
-          !err ? reply(docs) : reply(JSON.stringify(err));
-        });
-      }
+    handler: function (request, reply) {
+      DAL.properties.remove(request.params.id, function (err, docs) {
+        !err ? reply(docs) : reply(JSON.stringify(err));
+      });
     }
   });
 

7d6c092f254d4b5d1de83603661dc1ff5e2a22f9
vetalcore <vetalcore@gmail.com>
2016-09-04 22:41:11 +0300

    CART-96 Make header be responsive or not

--- dev/publicDev/headerDirective/headerDirective.js
+++ dev/publicDev/headerDirective/headerDirective.js
@@ -4,6 +4,9 @@ var app = angular.module('app');
 
 app.directive('headerDirective', function() {
   return {
+    scope: {
+      className: '='
+    },
     templateUrl: 'headerDirective/headerDirective.html'
   }
 })

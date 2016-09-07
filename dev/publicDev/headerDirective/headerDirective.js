ebf22307548c7bf8da9cdb75d009dcdefa240240
Feditskiy Roman <feditskiy@gmail.com>
2016-09-07 13:53:50 +0300

    CART-95 fixed logout functionality

--- dev/publicDev/headerDirective/headerDirective.js
+++ dev/publicDev/headerDirective/headerDirective.js
@@ -2,8 +2,23 @@
 
 var app = angular
   .module('app')
-  .directive('headerDirective', function() {
+  .directive('headerDirective', ['TokenService', '$location', function() {
     return {
-      templateUrl: 'headerDirective/headerDirective.html'
+      templateUrl: 'headerDirective/headerDirective.html',
+      controller: HeaderController,
+      controllerAs: 'vm'
     }
-  });
+  }]);
+
+var HeaderController = function(TokenService, $location) {
+  var vm = this;
+
+  vm.logout = function() {
+    console.log(1111);
+    TokenService.removeToken();
+    $location.path('/');
+    console.log('2222');
+  };
+};
+
+HeaderController.$inject = ['TokenService', '$location'];

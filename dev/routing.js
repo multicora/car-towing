1c1b12beb2c857ed28cd764ac572f4e4c2bfaae7
Dmytro Borchuk <frunk.lern@gmail.com>
2016-09-11 01:16:06 +0300

    CART-123 Move properties routing

--- dev/routing.js
+++ dev/routing.js
@@ -37,24 +37,6 @@ module.exports.init = function (server) {
       reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
   });
-  server.route({
-    method: 'GET',
-    path: '/test',
-    handler: function (request, reply) {
-      DAL.properties.get(function (err, docs) {
-        reply('Docs: ' + JSON.stringify(docs));
-      });
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/testcreate',
-    handler: function (request, reply) {
-      DAL.properties.create({name: 'testName'}, function (err, docs) {
-        !err ? reply('Done') : reply('Error:');
-      });
-    }
-  });
 
   // Custom pages
   server.route({
@@ -97,83 +79,6 @@ module.exports.init = function (server) {
     }
   });
 
-  // Properties
-  server.route({
-    method: 'GET',
-    path: '/api/properties',
-    handler: function (request, reply) {
-      DAL.properties.get(function (err, docs) {
-        !err ? reply(docs) : reply(JSON.stringify(err));
-      });
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/api/property/{id}',
-    config: { 
-      pre: [
-        { method: 'checkTokin(raw.req.headers.token)', assign: "token" }
-      ],
-      handler: function (request, reply) {
-        DAL.properties.getById(request.params.id, function (err, docs) {
-          !err ? reply(docs) : reply(JSON.stringify(err));
-        });
-      }
-    }
-  });
-  server.route({
-    method: 'POST',
-    path: '/api/property',
-    config: {
-      validate: {
-        payload: {
-          name: Joi.string().min(1).max(255).required(),
-          address: Joi.string(),
-          logo: Joi.string(),
-          location: Joi.string(),
-          blocked: Joi.boolean()
-        }
-      },
-      handler: function (request, reply) {
-        DAL.properties.create(request.payload, function (err, docs) {
-          !err ? reply('Done') : reply(JSON.stringify(err));
-        });
-      }
-    }
-  });
-
-  /*Content-Type: application/x-www-form-urlencoded*/
-  server.route({
-    method: 'PUT',
-    path: '/api/property/{id}',
-    config: {
-      validate: {
-        params: {
-          name: Joi.string().min(1).max(255).required(),
-          address: Joi.string(),
-          objId: Joi.string(),
-          logo: Joi.string(),
-          rules: Joi.array().items(Joi.string())
-        }
-      },
-      handler: function (request, reply) {
-        DAL.properties.edit(request.params.id, request.payload, function (err, docs) {
-          !err ? reply(docs) : reply(JSON.stringify(err));
-        });
-      }
-    }
-  });
-
-  server.route({
-    method: 'DELETE',
-    path: '/api/property/{id}',
-    handler: function (request, reply) {
-      DAL.properties.remove(request.params.id, function (err, docs) {
-        !err ? reply(docs) : reply(JSON.stringify(err));
-      });
-    }
-  });
-
   server.route({
     method: 'POST',
     path: '/api/login',
@@ -201,6 +106,7 @@ module.exports.init = function (server) {
   // Blocking
   require('./routing/blocking.js')(server);
   require('./routing/parkingRules.js')(server);
+  require('./routing/properties.js')(server);
 
   server.route({
     method: 'GET',

db2a804cd5b61d1fef9c35a4e6811623943af3c4
Feditskiy Roman <feditskiy@gmail.com>
2016-09-15 16:28:35 +0300

    CART-37 adde ui for towed car page

---
+++ dev/publicDev/towed-car-page/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('app');
+
+app.
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when(
+    '/towed_car',
+    {
+      templateUrl: 'towed-car-page/towed-car-page.html',
+      // controller: 'towedСarCtrl as vm'
+    }
+  );
+}]);
---
+++ dev/publicDev/towed-car-page/towed-carCtrl.js
@@ -0,0 +1,8 @@
+// 'use strict';
+// function towedСarCtrl(Service) {
+//   var vm = this;
+
+
+// }
+// // towedСarCtrl.$inject = ['Service'];
+// app.controller('towedСarCtrl', towedСarCtrl);

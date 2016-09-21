f21dbce2e90bcf15c2ebcb8be2bbb1ca4b2f387c
Feditskiy Roman <feditskiy@gmail.com>
2016-09-21 13:34:14 +0300

    CART-136 added validation for user login

--- dev/routing/auth.js
+++ dev/routing/auth.js
@@ -4,6 +4,7 @@ const path = require('path');
 const Boom = require('boom');
 const DAL = require('../dal/dal.js');
 const Utils = require('../services/utils.js');
+const Joi = require('joi');
 
 module.exports = function (server) {
   server.route({
@@ -19,18 +20,26 @@ module.exports = function (server) {
   server.route({
     method: 'POST',
     path: '/api/login',
-    handler: function (request, reply) {
-      const user = request.payload;
-      DAL.users.getUserByEmail(user.login, (err, docs) => {
-        if (!!docs && docs.password == user.password) {
-            let token = Utils.newToken();
-            DAL.users.updateToken(token, user.login, (err, user) => {
-              !!user ? reply(user) : reply(Boom.badImplementation('Server error'));
-            });
-        } else {
-          reply(Boom.unauthorized('The username or password is incorrect'));
+    config: {
+      validate: {
+        payload: {
+          login: Joi.string().email().min(3).max(255).required(),
+          password: Joi.string().required()
         }
-      });
+      },
+      handler: function (request, reply) {
+        const user = request.payload;
+        DAL.users.getUserByEmail(user.login, (err, docs) => {
+          if (!!docs && docs.password == user.password) {
+              let token = Utils.newToken();
+              DAL.users.updateToken(token, user.login, (err, user) => {
+                !!user ? reply(user) : reply(Boom.badImplementation('Server error'));
+              });
+          } else {
+            reply(Boom.unauthorized('The username or password is incorrect'));
+          }
+        });
+      }
     }
   });
 };

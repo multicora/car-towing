f7a846238bca0ceb68783463544831adff2f785d
Feditskiy Roman <feditskiy@gmail.com>
2016-09-26 14:31:13 +0300

    CART - 121 fixed

--- dev/publicDev/scripts/userCheckingService.js
+++ dev/publicDev/scripts/userCheckingService.js
@@ -14,21 +14,13 @@
       }
 
       function checkUser(user, action) {
-        // var length = user.roles.actions.length;
-        // var i = 0;
-        // for (; i < length; i++) {
-        //   user.roles.actions[i] === action ? return true : return false;
-        // }
-
-        var answer = false;
-        if (user.roles === 'admin' && action === 'see-admin-page') {
-          answer = true;
-        } else {
-          if(user.roles === 'manager' && action === 'see-manager-page') {
-            answer = true;
-          } else {
-            if(user.roles === 'user' && action === 'see-user-page') {
-            answer = true;
+        var map = {
+          admin: ['see-admin-page'],
+          manager: ['see-manager-page']
+        };
+        for (var key in map) {
+          if (key === user.role) {
+            var answer = map[key].indexOf(action) >= 0;
           }
         }
         return answer;

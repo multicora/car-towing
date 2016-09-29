433386cdd101f3def8ccf9ede1fe06f7f448c9ab
Feditskiy Roman <feditskiy@gmail.com>
2016-09-29 16:48:51 +0300

    CART-138 added

--- dev/publicDev/scripts/resolver.js
+++ dev/publicDev/scripts/resolver.js
@@ -5,20 +5,22 @@
 
   app.service('resolver', customPageService);
 
-  customPageService.$inject = ['$q', 'authService'];
+  customPageService.$inject = ['$q', 'authService', 'TokenService'];
 
-  function customPageService($q, authService) {
+  function customPageService($q, authService, TokenService) {
     this.get = function () {
       return  $q(function (resolve) {
-        authService.getCurrentUser().then(
-          function (res) {
-            authService.setUser(res.data);
-            resolve();
-          },
-          function (errRes) {
-            resolve();
-          }
-        );
+        if (TokenService.getToken()) {
+          authService.getCurrentUser().then(
+            function (res) {
+              authService.setUser(res.data);
+              resolve();
+            },
+            function (errRes) {
+              resolve();
+            }
+          );
+        }
       })
     }
   }

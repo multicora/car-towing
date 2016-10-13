f6299f281ebec1e0250736d2a1c693dee89cff96
FeditskiyRoman <feditskiy@gmail.com>
2016-10-13 13:40:39 +0300

    CART-132 fixed

--- dev/publicDev/scripts/Interseptor.js
+++ dev/publicDev/scripts/Interseptor.js
@@ -10,12 +10,20 @@
     function Interseptor(LoadingService) {
       var loadingMessage = {
           request: function(request) {
-            LoadingService.showSpinner();
-            return request;
+            LoadingService.showSpinner()
+              .then(function(success) {
+                return request;
+              }, function(err) {
+                console.log(err);
+              });
           },
           response: function(response) {
-            LoadingService.hideSpinner();
-            return response;
+            LoadingService.hideSpinner()
+              .then(function(success) {
+                return response;
+              }, function(err) {
+                console.log(err);
+              });
           }
       };
       return loadingMessage;

d1fba89354d0a0353311115508ab2fc12be3e646
FeditskiyRoman <feditskiy@gmail.com>
2016-10-14 14:59:09 +0300

    fixed

--- dev/publicDev/scripts/Interseptor.js
+++ dev/publicDev/scripts/Interseptor.js
@@ -11,19 +11,11 @@
       var loadingMessage = {
           request: function(request) {
             LoadingService.showSpinner()
-              .then(function(success) {
                 return request;
-              }, function(err) {
-                console.log(err);
-              });
           },
           response: function(response) {
             LoadingService.hideSpinner()
-              .then(function(success) {
                 return response;
-              }, function(err) {
-                console.log(err);
-              });
           }
       };
       return loadingMessage;

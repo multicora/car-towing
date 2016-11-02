a644b93aa98e27f5a33abd0075341bfe06435d36
Roman Fedytskyi <feditskiy@gmail.com>
2016-11-02 22:26:04 +0200

    Feature/cart 159 edit property (#42)
    * CART-159 added adminPropertiesEditCtrl.js
    
    * CART-159 added edit controller
    
    * CART-159 added button edit
    
    * CART-159
    
    * CART-159 fixed
    
    * CART-159 finished

--- dev/publicDev/properties/propertiesService.js
+++ dev/publicDev/properties/propertiesService.js
@@ -30,6 +30,9 @@ function service ($http, TokenService) {
     getPhotoPath: function(photoPath) {
       return photoPath + '?' + TokenService.getTokenName() + '=' + TokenService.getToken();
     },
+    update: function(id, property) {
+      return $http.put('api/property/' + id + '?' + TokenService.getTokenName() + '=' + TokenService.getToken(), property);
+    },
     remove: function(id) {
       return $http.delete('/api/property/' + id, {
           headers: {Authorization: TokenService.getTokenName() + ' ' + TokenService.getToken()}

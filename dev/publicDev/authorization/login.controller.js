80eb124b2d1fb22662c5547abd68ef6c14ce2ede
FeditskiyRoman <feditskiy@gmail.com>
2017-01-23 11:16:53 +0200

    CART-237 fixed

--- dev/publicDev/authorization/login.controller.js
+++ dev/publicDev/authorization/login.controller.js
@@ -56,7 +56,15 @@
     }
 
     vm.resetPassword = function() {
-      authService.resetPassword({email: vm.email});
+      authService.resetPassword({email: vm.email}).then(
+        function(success) {
+          if(!success || success.status != 200) {
+            vm.errorResetMessage = "User doesn't exists";
+          } else {
+            vm.errorResetMessage = "";
+          }
+        }
+      );
     }
 
     function redirectByRole(roles) {

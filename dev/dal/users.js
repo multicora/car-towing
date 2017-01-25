596a5e2cb4ee0798b5f1aaa98eb0821fcf6efb13
FeditskiyRoman <feditskiy@gmail.com>
2017-01-25 11:19:03 +0200

    CART-244 added number user to db

--- dev/dal/users.js
+++ dev/dal/users.js
@@ -12,7 +12,11 @@ const schema = new Schema({
   token: { type: String, max: 255 },
   resetToken: {type: String, max: 255 },
   blocked: Boolean,
-  roles: [ {type: Schema.Types.ObjectId, ref: 'roles'} ]
+  roles: [ {type: Schema.Types.ObjectId, ref: 'roles'} ],
+  number: {
+    type: String,
+    unique: true
+  }
 });
 
 let model = mongoose.model(path.basename(module.filename, '.js'), schema);

e685566290861a347b508f6410cb31c589d39d65
FeditskiyRoman <feditskiy@gmail.com>
2017-02-13 22:21:29 +0200

    CART-277 fixed

--- dev/routing/property.js
+++ dev/routing/property.js
@@ -133,7 +133,7 @@ module.exports = function (server) {
       handler: function handler(request, reply) {
         const propertyId = request.payload.propertyId;
         const managerEmail = request.payload.email;
-        const serverUrl = request.headers.header;
+        const serverUrl = request.headers.referer;
         let userExist = false;
 
         const createOrGetUser = (newUser, cb) => {

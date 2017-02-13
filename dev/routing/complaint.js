144ccecb6df18e60dd3809dd3958808a58f79668
FeditskiyRoman <feditskiy@gmail.com>
2017-02-13 15:11:21 +0200

    CART-287 remove email

--- dev/routing/complaint.js
+++ dev/routing/complaint.js
@@ -21,7 +21,7 @@ module.exports = function (server) {
       ].join('\n');
       const mail = {
         from: '"No-reply" <no.reply.ultimatetowing@gmail.com>', // sender address
-        to: 'no.reply.ultimatetowing@gmail.com', // list of receivers
+        to: config.mail, // list of receivers
         subject: 'Complaint from ' + request.payload.name, // Subject line
         text: message, // plaintext body
         html: '<div style="white-space: pre;">' + message + '</div>' // html body

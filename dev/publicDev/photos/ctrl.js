a4a7fe0b28cfe8848ad895a8b2c53bd5805654a8
dmitrij-borchuk <frunk.lern@gmail.com>
2017-03-22 21:16:40 -0700

    CART-295 Manager cant see photo page (#96)

--- dev/publicDev/photos/ctrl.js
+++ dev/publicDev/photos/ctrl.js
@@ -36,9 +36,11 @@
 
         return Promise.all(vm.promiseArr);
       }).then(function (users) {
+        var time;
 
         for (var i = 0; i < vm.photos.length; i++) {
-          vm.photos[i].updated = new Date(vm.photos.updated);
+          time = new Date(vm.photos[i].updated);
+          vm.photos[i].updated = time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
           vm.photos[i].ownerNumber = users[i].data.number;
         }
 
